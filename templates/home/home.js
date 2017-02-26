a586506b3d25e92d479dacbf6b1c4735cff92021
thisistheaj <ajbeckner@gmail.com>
2017-02-26 11:13:31 -0500

    posting posts works

--- templates/home/home.js
+++ templates/home/home.js
@@ -4,9 +4,19 @@
     angular.module('app.controllers').controller('HomeCtrl', ['$scope','Service', HomeCtrl]);
 
     function HomeCtrl($scope,Service) {
+        // $scope.createPost = createPost;
         Service.getPosts().then(function (response) {
             $scope.data = response.data;
-        })
+        });
+
+        // function createPost() {
+            Service.addPost("bartitle").then(function (res) {
+                console.log('success');
+                console.log(res);
+            },function (err) {
+                console.log(err);
+            })
+        // }
     }
 
 })();
